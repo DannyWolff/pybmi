33bccd5be6fade5b82cb49429f402eb6ac9c7148
Bogdan Lupu <bogdanlupu.mail@gmail.com>
2018-10-10 06:32:13 +0300

    Created Main Menu

---
+++ main.py
@@ -0,0 +1,31 @@
+# # measurment = input('Please choose  your measurment system: Metric or imperial')
+# print('Please choose  your measurment system: Metric or Imperial')
+
+
+
+# height =float(input('Enter your height '))
+def main_menu():
+
+    choice ='0'
+    while choice =='0':
+        print("Main Choice: Choose Measurments")
+        print("1 Metric")
+        print("2 Imperial")
+        print("3 Quit")
+
+        choice = input ("Please make a choice: ")
+
+        if choice == "1":
+            print("Metric")
+        elif choice == "2":
+            print("Imperial")
+        elif choice == "3":
+            break
+        else:
+            print("Please enter 1 or 2.")
+            main_menu()
+
+def second_menu():
+    pass
+
+main_menu()
